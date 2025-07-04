a6798e2ec9dfd04fe977aeb4e1a5367878542a24
Dennis Erazo <dennis.erazo96@gmail.com>
2025-07-04 11:20:27 -0600

    Resolucion de conflictos

--- lib/config/presentation/widgets/movies/movies_slideshow.dart
+++ lib/config/presentation/widgets/movies/movies_slideshow.dart
@@ -5,50 +5,41 @@ import 'package:cinemapedia_app/config/theme/theme_context.dart';
 import 'package:flutter/material.dart';
 
 class MoviesSlideshow extends StatelessWidget {
-
   final List<Movie> movies;
 
-  const MoviesSlideshow({
-    super.key, 
-    required this.movies
-  });
+  const MoviesSlideshow({super.key, required this.movies});
 
   @override
   Widget build(BuildContext context) {
-
     final colors = getColorScheme(context);
 
     return SizedBox(
       width: double.infinity,
       height: 260,
       child: Swiper(
-        pagination:  SwiperPagination(
-          margin: const EdgeInsets.only(top: 10),
-          builder: DotSwiperPaginationBuilder(
-            color: colors.inversePrimary,
-            activeColor: colors.primary,
-          )
-        ),
+        pagination: SwiperPagination(
+            margin: const EdgeInsets.only(top: 10),
+            builder: DotSwiperPaginationBuilder(
+              color: colors.inversePrimary,
+              activeColor: colors.primary,
+            )),
         viewportFraction: 0.8,
         scale: 0.9,
         autoplay: true,
         itemCount: movies.length,
         itemBuilder: (context, index) => _Slide(movie: movies[index]),
-        
       ),
     );
   }
 }
 
 class _Slide extends StatelessWidget {
-
   final Movie movie;
 
   const _Slide({required this.movie});
 
   @override
   Widget build(BuildContext context) {
-
     final decoration = BoxDecoration(
       borderRadius: BorderRadius.circular(30),
       boxShadow: const [
@@ -59,20 +50,19 @@ class _Slide extends StatelessWidget {
           //spreadRadius: 1,
         ),
       ],
-      
     );
 
     return Padding(
-    padding: const EdgeInsets.only(bottom: 30),
-    child: DecoratedBox(
-      decoration: decoration,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: Image.network(
-          movie.backdropPath,
-          fit: BoxFit.cover,
-          loadingBuilder: (context, child, loadingProgress) {
-            /*if (loadingProgress == null) return child;
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(
+            movie.backdropPath,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              /*if (loadingProgress == null) return child;
             return Center(
               child: CircularProgressIndicator(
                 value: loadingProgress.expectedTotalBytes != null
@@ -80,15 +70,15 @@ class _Slide extends StatelessWidget {
                     : null,
               ),
             );*/
-            if (loadingProgress != null){
-              return const DecoratedBox(
-                decoration: BoxDecoration(color: Colors.black12));
-            }
-          
-            return FadeIn(child: child);
-          },
+              if (loadingProgress != null) {
+                return const DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.black12));
+              }
+
+              return FadeIn(child: child);
+            },
+          ),
         ),
-      ),
       ),
     );
   }
